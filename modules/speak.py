6e8044f38b792edd532835f23abf35750207fec2
CodeZhifty <juulstausholmoscar@gmail.com>
2024-05-28 13:08:54 +0200

    Changed to male voice

--- modules/speak.py
+++ modules/speak.py
@@ -2,7 +2,7 @@ import pyttsx3
 def speak(input):
     engine = pyttsx3.init()
     voices = engine.getProperty('voices')
-    engine.setProperty("voice", voices[1].id)
-    engine.setProperty("rate", 125)
+    engine.setProperty("voice", voices[0].id)
+    engine.setProperty("rate", 150)
     engine.say(input)
     engine.runAndWait()
